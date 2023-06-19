eea8bdbd7cae3f57e4620e194587a1171ed9bb98
Ivan Feschenko <zorromen84@gmail.com>
2023-06-19 22:18:50 +0300

    2.0.0.5.8

--- samples/generator2/app/src/main/java/com/example/generator2/screens/config/atom/ConfigEdit.kt
+++ samples/generator2/app/src/main/java/com/example/generator2/screens/config/atom/ConfigEdit.kt
@@ -1,9 +1,13 @@
 package com.example.generator2.screens.config.atom
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
@@ -11,23 +15,25 @@ import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-@Composable
-public fun TextFieldDefaults.colorsEdit(
-    textColor: Color = Color.White,
-    focusedBorderColor: Color = Color.LightGray,
-    focusedLabelColor: Color = Color.White,
-): TextFieldColors =
-    outlinedTextFieldColors(
-        textColor = textColor,
-        focusedBorderColor = focusedBorderColor,
-        focusedLabelColor = focusedLabelColor
-    )
+//@Composable
+//public fun TextFieldDefaults.colorsEdit(
+//    textColor: Color = Color.White,
+//    focusedBorderColor: Color = Color.LightGray,
+//    focusedLabelColor: Color = Color.White,
+//): TextFieldColors =
+//    outlinedTextFieldColors(
+//        textColor = textColor,
+//        focusedBorderColor = focusedBorderColor,
+//        focusedLabelColor = focusedLabelColor
+//    )
 
 @Composable
 fun editConfig(
@@ -35,54 +41,80 @@ fun editConfig(
     label: String,
     value: State<Float>,
     onDone: (Float) -> Unit = {},
-    min : Float,
-    max : Float,
-    toInt : Boolean = false
+    min: Float,
+    max: Float,
+    toInt: Boolean = false
 
 ) {
 
-    var text by remember { mutableStateOf(TextFieldValue(if (toInt) value.value.toInt().toString() else value.value.toString()) ) }
+    var text by remember {
+        mutableStateOf(
+            TextFieldValue(
+                if (toInt) value.value.toInt().toString() else value.value.toString()
+            )
+        )
+    }
     val localFocusManager = LocalFocusManager.current
     val focusRequester = remember { FocusRequester() }
 
-    OutlinedTextField(
-        colors = TextFieldDefaults.colorsEdit(),
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp)
-            .then(modifier).focusRequester ( focusRequester ),
-
-        value =  text , //if (temp.text.isNotEmpty()) {value as TextFieldValue} else {"" as TextFieldValue}  ,
-        label = { Text(text = label, fontSize = 16.sp) },
+    BasicTextField(
+        value = text,
         onValueChange = {
             text = it
         },
-        keyboardOptions = KeyboardOptions( imeAction = ImeAction.Done, keyboardType = KeyboardType.Decimal ),
+
+        modifier = Modifier
+            .padding(8.dp)
+            .then(modifier)
+            .focusRequester(focusRequester)
+            .background(color = Color(0xFF353838), shape = RoundedCornerShape(size = 16.dp))
+            .border(
+                width = 1.dp,
+                color = Color(0xFF696B6B),
+                shape = RoundedCornerShape(size = 16.dp)
+            )
+
+        ,
+
+        keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Done,
+            keyboardType = KeyboardType.Decimal
+        ),
+
         keyboardActions = KeyboardActions(
             onDone = {
 
                 if (text.text == "")
                     text = TextFieldValue(text = min.toString())
 
-                var s = text.text.replace("-","").replace(",","").trim()
+                var s = text.text.replace("-", "").replace(",", "").trim()
                 if (s.isEmpty()) s = min.toString()
 
                 try {
                     var f = s.toFloat()
                     if (f > max) f = max
                     if (f < min) f = min
-                    text  = TextFieldValue(text = if (toInt) f.toInt().toString() else f.toString() )
+                    text = TextFieldValue(text = if (toInt) f.toInt().toString() else f.toString())
                     onDone(f)
-                }
-                catch ( e:Exception)
-                {
+                } catch (e: Exception) {
                     onDone(min)
                 }
                 localFocusManager.clearFocus()
             }
         ),
-        textStyle = TextStyle (fontSize = 18.sp)
-    )
+
+
+        textStyle = TextStyle(
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Medium,
+            color = Color.White,
+            textAlign = TextAlign.Center
+        ),
+
+
+        )
+
+
 }
 
 
--- samples/generator2/app/src/main/java/com/example/generator2/screens/config/molecule/ConfigConstrain.kt
+++ samples/generator2/app/src/main/java/com/example/generator2/screens/config/molecule/ConfigConstrain.kt
@@ -1,162 +1,95 @@
 package com.example.generator2.screens.config.molecule
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Divider
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableFloatState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.example.generator2.model.LiveConstrain
 import com.example.generator2.screens.config.Config_header
 import com.example.generator2.screens.config.atom.editConfig
 import com.example.generator2.screens.config.vm.VMConfig
 
+
 @Composable
-fun ConfigConstrain(vm: VMConfig)
-{
+private fun EditConfig(
+    text: String,
+    minCR: MutableFloatState,
+    vm: VMConfig,
+    toInt: Boolean = true
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
 
-    //CR min max
-    Row(modifier = Modifier.fillMaxWidth()) {
-        val minCR = LiveConstrain.minCR
+        Text(
+            text = text, color = Color.LightGray,
+            maxLines = 3,
+            minLines = 1,
+            modifier = Modifier.weight(1f)
+        )
+
         editConfig(
-            Modifier.weight(1f), "min CR", value = minCR, min = 50f, max = 10000f, toInt = true,
+            Modifier.width(120.dp), "", value = minCR, min = 0f, max = 10000f, toInt = toInt,
             onDone = {
-                LiveConstrain.minCR.value = it
-                vm.toastText("min CR Saved")
+                minCR.floatValue = it
+                vm.toastText("saved")
                 vm.saveConstrain()
             })
 
-        val maxCR = LiveConstrain.maxCR
-        editConfig(
-            Modifier.weight(1f), "max CR", value = maxCR, min = 50f, max = 10000f, toInt = true,
-            onDone = {
-                LiveConstrain.maxCR.value = it
-                vm.toastText("max CR Saved")
-                vm.saveConstrain()
-            })
     }
+}
 
-    //FMBase min max
-    Row(modifier = Modifier.fillMaxWidth()) {
-        val minFMBase = LiveConstrain.minFMBase
-        editConfig(Modifier.weight(1f), "min FMBase", value = minFMBase, min = 50f, max = 10000f, toInt = true,
-            onDone = {
-                LiveConstrain.minFMBase.value = it
-                vm.toastText("min FM Base Saved")
-                vm.saveConstrain()
-            })
 
-        val maxFMBase = LiveConstrain.maxFMBase
-        editConfig(Modifier.weight(1f), "max FMBase", value = maxFMBase, min = 50f, max = 10000f, toInt = true,
-            onDone = {
-                LiveConstrain.maxFMBase.value = it
-                vm.toastText("max FM Base Saved")
-                vm.saveConstrain()
-            })
-    }
+@Composable
+fun ConfigConstrain(vm: VMConfig) {
 
-    //FMDev min max
-    Row(modifier = Modifier.fillMaxWidth()) {
-        val minFMDev = LiveConstrain.minFMDev
-        editConfig(Modifier.weight(1f), "min FMDev", value = minFMDev, min = 0f, max = 10000f, toInt = true,
-            onDone = {
-                LiveConstrain.minFMDev.value = it
-                vm.toastText("min FM Dev Saved")
-                vm.saveConstrain()
-            })
 
-        val maxFMDev = LiveConstrain.maxFMDev
-        editConfig(Modifier.weight(1f), "max FMDev", value = maxFMDev, min = 0f, max = 10000f, toInt = true,
-            onDone = {
-                LiveConstrain.maxFMDev.value = it
-                vm.toastText("max FM Dev Saved")
-                vm.saveConstrain()
-            })
-    }
+    EditConfig("Минимальная частота несущей", LiveConstrain.minCR, vm)
+    EditConfig("Максимальная частота несущей", LiveConstrain.maxCR, vm)
 
-    //ModAmFm min max
-    Row(modifier = Modifier.fillMaxWidth()) {
-        val minModAmFm = LiveConstrain.minModAmFm
-        editConfig(Modifier.weight(1f), "min Mod Am Fm", value = minModAmFm, min = 0f, max = 1000f, toInt = false,
-            onDone = {
-                LiveConstrain.minModAmFm.value = it
-                vm.toastText("min Mod AmFm Saved")
-                vm.saveConstrain()
-            })
 
-        val maxModAmFm = LiveConstrain.maxModAmFm
-        editConfig(Modifier.weight(1f), "max Mod Am Fm", value = maxModAmFm, min = 0f, max = 1000f, toInt = false,
-            onDone = {
-                LiveConstrain.maxModAmFm.value = it
-                vm.toastText("max Mod AmFm Saved")
-                vm.saveConstrain()
-            })
-    }
+    EditConfig("Минимальная частота FM девиации", LiveConstrain.minFMDev, vm)
+    EditConfig("Максимальная частота FM девиации", LiveConstrain.maxFMDev, vm)
+
+    EditConfig("Минимальная частота AM модуляции", LiveConstrain.minModAmFm, vm)
+    EditConfig("Максимальная частота AM модуляции", LiveConstrain.maxModAmFm, vm)
+
 
     Divider()
-    Config_header("Sensitivity Slider")
+    Config_header("Чувствительность слайдера")
 
     //Чувствительность слайдера
     //var sensetingSliderCr =  mutableStateOf( 0.2f)
-   // var sensetingSliderFmDev =  mutableStateOf( 0.2f)
-   // var sensetingSliderFmBase =  mutableStateOf( 0.2f)
-  //  var sensetingSliderAmFm =  mutableStateOf( 0.01f)
+    // var sensetingSliderFmDev =  mutableStateOf( 0.2f)
+    // var sensetingSliderFmBase =  mutableStateOf( 0.2f)
+    //  var sensetingSliderAmFm =  mutableStateOf( 0.01f)
 
-    Row(modifier = Modifier.fillMaxWidth()) {
-        val sensetingSliderCr = LiveConstrain.sensetingSliderCr
-        editConfig(Modifier.weight(1f),
-            "Carrier 0.2",
-            value = sensetingSliderCr,
-            min = 0f,
-            max = 1f,
-            toInt = false,
-            onDone = {
-                LiveConstrain.sensetingSliderCr.value = it
-                vm.toastText("sensitivity Slider Carrier Saved")
-                vm.saveConstrain()
-            })
-
-        val sensetingSliderAmFm = LiveConstrain.sensetingSliderAmFm
-        editConfig(Modifier.weight(1f),
-            "Am Fm 0.01",
-            value = sensetingSliderAmFm,
-            min = 0f,
-            max = 1f,
-            toInt = false,
-            onDone = {
-                LiveConstrain.sensetingSliderAmFm.value = it
-                vm.toastText("sensitivity Slider Am Fm Saved")
-                vm.saveConstrain()
-            })
-    }
-
-    Row(modifier = Modifier.fillMaxWidth()) {
-        val sensetingSliderFmBase = LiveConstrain.sensetingSliderFmBase
-        editConfig(Modifier.weight(1f),
-            "Fm Base 0.2",
-            value = sensetingSliderFmBase,
-            min = 0f,
-            max = 1f,
-            toInt = false,
-            onDone = {
-                LiveConstrain.sensetingSliderFmBase.value = it
-                vm.toastText("sensitivity Slider Fm Base Saved")
-                vm.saveConstrain()
-            })
-
-        val sensetingSliderFmDev = LiveConstrain.sensetingSliderFmDev
-        editConfig(Modifier.weight(1f),
-            "Fm Dev 0.2",
-            value = sensetingSliderFmDev,
-            min = 0f,
-            max = 1f,
-            toInt = false,
-            onDone = {
-                LiveConstrain.sensetingSliderFmDev.value = it
-                vm.toastText("sensitivity Slider Fm Dev Saved")
-                vm.saveConstrain()
-            })
-    }
-
-
+    EditConfig(
+        "Чувствительность слайдера несущая (0.2)",
+        LiveConstrain.sensetingSliderCr,
+        vm,
+        toInt = false
+    )
+    EditConfig(
+        "Чувствительность слайдера AM FM модуляции (0.01)",
+        LiveConstrain.sensetingSliderAmFm,
+        vm,
+        toInt = false
+    )
+    EditConfig(
+        "Чувствительность слайдера FM девиации (0.2)",
+        LiveConstrain.sensetingSliderFmDev,
+        vm,
+        toInt = false
+    )
 
 }
