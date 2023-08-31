d75e5045d132abf38ebced4d7d1fc127e11e5825
Sacamoto84 <zorromen@mail.ru>
2023-08-31 12:32:36 +0300

    2.0.0.7.79

--- samples/generator2/app/src/main/java/com/example/generator2/scope/OscilloscopeControl.kt
+++ samples/generator2/app/src/main/java/com/example/generator2/scope/OscilloscopeControl.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -18,36 +17,33 @@ import com.example.generator2.mp3.OSCILLSYNC
 import com.example.generator2.mp3.oscillSync
 
 
-val m = Modifier.height(40.dp).width(40.dp).border(1.dp, Color.Gray).background(Color.Black)
-val m2 = Modifier.height(40.dp).width(40.dp).border(1.dp, Color.Gray).background(Color.Black)
+val m = Modifier.height(32.dp).width(32.dp).border(1.dp, Color.Gray).background(Color.Black)
 
-
+val colorEnabled = Color(0xFF004000)
+val colorTextEnabled = Color.White
+val colorTextDisabled = Color.DarkGray
 
 @Composable
 fun OscilloscopeControl() {
 
-    //val context = LocalContext.current
-    val h = 32.dp
-    val w = 64.dp
-
     Row {
 
-        Box(modifier = m.clickable(onClick = { oscillSync.value = OSCILLSYNC.NONE  }),
+        Box(modifier = m.clickable(onClick = { oscillSync.value = OSCILLSYNC.NONE  }).background(if (oscillSync.value == OSCILLSYNC.NONE ) colorEnabled else Color.Black),
             contentAlignment = Alignment.Center
         ) {
-            Text(text = "N", color = Color.Gray)
+            Text(text = "N", color = if (oscillSync.value == OSCILLSYNC.NONE ) colorTextEnabled else colorTextDisabled)
         }
 
-        Box(modifier = m.clickable(onClick = { oscillSync.value = OSCILLSYNC.L  }),
+        Box(modifier = m.clickable(onClick = { oscillSync.value = OSCILLSYNC.L  }).background(if (oscillSync.value == OSCILLSYNC.L ) colorEnabled else Color.Black),
             contentAlignment = Alignment.Center
         ) {
-            Text(text = "L", color = Color.Gray)
+            Text(text = "L", color = if (oscillSync.value == OSCILLSYNC.L ) colorTextEnabled else colorTextDisabled)
         }
 
-        Box(modifier = m.clickable(onClick = { oscillSync.value = OSCILLSYNC.R  }),
+        Box(modifier = m.clickable(onClick = { oscillSync.value = OSCILLSYNC.R  }).background(if (oscillSync.value == OSCILLSYNC.R ) colorEnabled else Color.Black),
             contentAlignment = Alignment.Center
         ) {
-            Text(text = "R", color = Color.Gray)
+            Text(text = "R", color = if (oscillSync.value == OSCILLSYNC.R ) colorTextEnabled else colorTextDisabled )
         }
 
     }
