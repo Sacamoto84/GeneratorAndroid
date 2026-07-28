c66f29ba12e600917f39528589381f20a5bfc213
Sacamoto84 <zorromen@mail.ru>
2026-07-28 07:08:35 +0300

    2.0.0.8.8

--- samples/generator2/app/src/main/java/com/example/generator2/features/noSQL/implNoSQL.kt
+++ samples/generator2/app/src/main/java/com/example/generator2/features/noSQL/implNoSQL.kt
@@ -1,7 +1,5 @@
 package com.example.generator2.features.noSQL
 
-import com.example.generator2.AppPath
-
 enum class KEY_NOSQL_CONFIG2(val value: String) {
     LANGUAGE("language"),
     UPDATEAUTO("updateauto") //Включение автообновления
